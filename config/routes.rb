541bfc0dd405e50b0ec9682a08f0aea504d46c94
itsuuki <53384302+itsuuki@users.noreply.github.com>
2020-01-12 16:02:37 +0900

    second-commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'mains/index'
+  devise_for :users
+  # get 'mains/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "mains#index"
 end
